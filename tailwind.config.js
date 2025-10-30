28068abcc094c4d45143bf5c151c924d27f0635f
francovillagra <fr4nconv@gmail.com>
2025-10-30 16:40:01 -0300

    Habilitando Dark Mode por clase en componente Tailwind.config.js en fila 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // 👈 habilita modo oscuro por clase
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -14,3 +15,4 @@ module.exports = {
   },
   plugins: [],
 }
+
