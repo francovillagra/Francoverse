7e4895481bc3a9f5facd9320a1fa541d08c2a139
francovillagra <fr4nconv@gmail.com>
2025-11-03 22:11:46 -0300

    Actualizando el index.ts: exportando todos los componentes

--- components/sections/Navigation/index.ts
+++ components/sections/Navigation/index.ts
@@ -1,7 +1,9 @@
+// components/sections/Navigation/index.ts
 export { default as TopNav } from "./TopNav";
 export { default as ThemeToggle } from "./ThemeToggle";
+export { default as HomeNavigation } from "./HomeNavigation";
+export { default as MobileNavigation } from "./MobileNavigation";
 export { default as NavigationMenu } from "./NavigationMenu";
 export { default as NavigationButtons } from "./NavigationButtons";
 export { default as NavigationItem } from "./NavigationItem";
-export { default as MobileNavigation } from "./MobileNavigation";
-export { default as HomeNavigation } from "./HomeNavigation";
+export { default as MainMenu } from "./MainMenu"; // ← nuevo en tu lista
