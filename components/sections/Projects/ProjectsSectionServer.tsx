a4a6271aa9b02ca6f6956fc60e7cf571478751aa
francovillagra <fr4nconv@gmail.com>
2026-04-20 16:25:35 -0300

    Actualizando componente ProjectsSectionServer.tsx: Para filtrar destacados y limitar en la home

--- components/sections/Projects/ProjectsSectionServer.tsx
+++ components/sections/Projects/ProjectsSectionServer.tsx
@@ -1,10 +1,32 @@
 import ProjectsSection from "./ProjectsSection";
 import { getGitHubProjects } from "@/lib/github";
 import { mapRepoToProject } from "@/lib/projects";
+import { projectMetadata } from "@/lib/project-metadata";
 
-export default async function ProjectsSectionServer() {
+type ProjectsSectionServerProps = {
+  limit?: number;
+  featuredOnly?: boolean;
+  showViewAll?: boolean;
+};
+
+export default async function ProjectsSectionServer({
+  limit,
+  featuredOnly = false,
+  showViewAll = false,
+}: ProjectsSectionServerProps) {
   const repos = await getGitHubProjects();
-  const projects = repos.map(mapRepoToProject);
 
-  return <ProjectsSection projects={projects} />;
+  const filteredRepos = featuredOnly
+    ? repos.filter((repo) => projectMetadata[repo.name]?.featured)
+    : repos;
+
+  const projects = filteredRepos.map(mapRepoToProject);
+
+  return (
+    <ProjectsSection
+      projects={projects}
+      limit={limit}
+      showViewAll={showViewAll}
+    />
+  );
 }
