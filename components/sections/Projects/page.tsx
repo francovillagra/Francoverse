5207819b83a3ca5bc6df93251f5dfe7e5b86f7e5
francovillagra <fr4nconv@gmail.com>
2026-04-21 12:38:33 -0300

    feat: crear la vista completa de proyectos y conectar el acceso desde la home

---
+++ components/sections/Projects/page.tsx
@@ -0,0 +1,6 @@
+// app/sections/projects/page.tsx
+import ProjectsSectionServer from "@/components/sections/Projects/ProjectsSectionServer";
+
+export default function ProjectsPage() {
+  return <ProjectsSectionServer />;
+}
