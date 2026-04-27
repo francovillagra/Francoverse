a0b2bdb0a851724388709593b20f9efc64911f74
francovillagra <fr4nconv@gmail.com>
2026-04-27 15:45:36 -0300

    Eliminando de components/sections/Navigation/index.ts: export { default as ThemeToggle } from './ThemeToggle'; export { default as MobileNavigation } from './MobileNavigation';

--- components/sections/Navigation/index.ts
+++ components/sections/Navigation/index.ts
@@ -1,7 +1,6 @@
 // components/sections/Navigation/index.ts
 export { default as TopNav } from "./TopNav";
 export { default as HomeNavigation } from "./HomeNavigation";
-export { default as MobileNavigation } from "./MobileNavigation";
 export { default as NavigationMenu } from "./NavigationMenu";
 export { default as NavigationButtons } from "./NavigationButtons";
 export { default as NavigationItem } from "./NavigationItem";
